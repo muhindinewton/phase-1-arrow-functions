8f0734ed5e956afe53fc92ba7bad8b858d0694d6
Newton Muhindi <muhindinewton@gmail.com>
2025-03-05 11:04:19 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,16 @@
+const divide = (a, b) => a / b;{
+    let a =2000
+    let b =100
+    console.log (a, b)
+}
+
+const square = x => x*x; {
+    let x = 2
+    console.log (x)
+}
+
+const add = (a, b) => a+b; {
+    let a = 3
+    let b = 4
+    console.log (a, b)
+}
